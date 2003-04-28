5fe219f0741c2e180d274109204ec287f68eba24
jani <jani>
2003-04-28 10:13:23 +0000

    add read and write to sio ops

--- ports/ecos/ecos/net/lwip_tcpip/current/src/ecos/sio.c
+++ ports/ecos/ecos/net/lwip_tcpip/current/src/ecos/sio.c
@@ -7,6 +7,7 @@
 #include "lwipopts.h"
 
 #include <cyg/io/io.h>
+#include <cyg/io/config_keys.h>
 
 static cyg_io_handle_t ser;
 
@@ -16,10 +17,6 @@ void sio_send(char c,void * dev)
 {
 	len = 1;
 	cyg_io_write(*(cyg_io_handle_t*)dev, &c, &len);
-
-//	if (len!=1)
-//		diag_printf("err send\n");
-
 }
 
 char sio_recv(void * dev)
@@ -27,21 +24,35 @@ char sio_recv(void * dev)
 	char c;
 	len = 1;
 	cyg_io_read(*(cyg_io_handle_t *)dev, &c, &len);
-
-//	if (len!=1)
-//		diag_printf("err recv\n");
-		
 	return c;			
 }
-	
+
+int sio_write(void *dev, char *b, int size)
+{
+	int len = size;
+	cyg_io_write(*(cyg_io_handle_t*)dev, b, &len);
+	return len;
+}
+		
+int sio_read(void *dev, char *b, int size)
+{
+	int len = size;
+	cyg_io_read(*(cyg_io_handle_t*)dev, b, &len);
+	return len;
+}
 void * sio_open(int devnum)
 {
 	int res;
+	cyg_uint32 nb = 0;
 	
-//		diag_printf("fun\n");
-	res = cyg_io_lookup(SLIP_DEV, &ser);
-//	if (res != ENOERR)
-//		diag_printf("shit\n");
+	char siodev[] = "/dev/serX";
+	if (devnum < 0 || devnum >9)
+		return NULL;
+	siodev[8] = '0' + devnum;
+	res = cyg_io_lookup(siodev, &ser);
+	if (res != ENOERR)
+		diag_printf("Cannot open %s\n", siodev);
+	cyg_io_set_config(ser, CYG_IO_SET_CONFIG_READ_BLOCKING, nb, 4);
 	return &ser; 
 }
 
