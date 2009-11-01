1a265b5373c5ddb4b0409fc37c03f7f75f6b0ec9
goldsimon <goldsimon>
2009-11-01 19:23:12 +0000

    Update fsdata (generated by makefsdata)

--- apps/httpserver_raw/fsdata.h
+++ apps/httpserver_raw/fsdata.h
@@ -37,6 +37,9 @@ struct fsdata_file {
   const unsigned char *name;
   const unsigned char *data;
   const int len;
+  u8_t includes_http_header;
+  u8_t content_type;
+  u8_t http_response;
 };
 
 #endif /* __FSDATA_H__ */
