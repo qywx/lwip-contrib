f4edb2aff377313aa92518df311d0d0610759f57
Dirk Ziegelmeier <dirk@ziegelmeier.net>
2016-06-29 22:10:22 +0200

    Compiling PPP Van Jacobsen code with clang was fixed by Sylvain

--- ports/unix/proj/unixsim/lwipopts.h
+++ ports/unix/proj/unixsim/lwipopts.h
@@ -289,12 +289,7 @@ extern void sntp_set_system_time(u32_t sec);
 #define MSCHAP_SUPPORT   0      /* Set > 0 for MSCHAP (NOT FUNCTIONAL!) */
 #define CBCP_SUPPORT     0      /* Set > 0 for CBCP (NOT FUNCTIONAL!) */
 #define CCP_SUPPORT      0      /* Set > 0 for CCP (NOT FUNCTIONAL!) */
-#ifdef __clang__
-/* Van Jacobson TCP header compression currently does not compile without warnings with CLANG */
-#define VJ_SUPPORT       0
-#else
 #define VJ_SUPPORT       1      /* Set > 0 for VJ header compression. */
-#endif
 #define MD5_SUPPORT      1      /* Set > 0 for MD5 (see also CHAP) */
 
 
