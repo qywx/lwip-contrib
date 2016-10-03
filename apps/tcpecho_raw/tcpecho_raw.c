ca51de0748bf81ef2a1b1612fa4f32b2ffbe1c66
Dirk Ziegelmeier <dirk@ziegelmeier.net>
2016-10-03 09:26:04 +0200

    Minor: Fix coding style in tcpecho_raw and udpecho_raw

--- apps/tcpecho_raw/tcpecho_raw.c
+++ apps/tcpecho_raw/tcpecho_raw.c
@@ -40,7 +40,6 @@
  *
  */
 
-
 #include "lwip/opt.h"
 #include "lwip/debug.h"
 #include "lwip/stats.h"
@@ -68,42 +67,186 @@ struct tcpecho_raw_state
   struct pbuf *p;
 };
 
-err_t tcpecho_raw_accept(void *arg, struct tcp_pcb *newpcb, err_t err);
-err_t tcpecho_raw_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err);
-void tcpecho_raw_error(void *arg, err_t err);
-err_t tcpecho_raw_poll(void *arg, struct tcp_pcb *tpcb);
-err_t tcpecho_raw_sent(void *arg, struct tcp_pcb *tpcb, u16_t len);
-void tcpecho_raw_send(struct tcp_pcb *tpcb, struct tcpecho_raw_state *es);
-void tcpecho_raw_close(struct tcp_pcb *tpcb, struct tcpecho_raw_state *es);
-void tcpecho_raw_free(struct tcpecho_raw_state *es);
-
-void
-tcpecho_raw_init(void)
+static void
+tcpecho_raw_free(struct tcpecho_raw_state *es)
 {
-  tcpecho_raw_pcb = tcp_new_ip_type(IPADDR_TYPE_ANY);
-  if (tcpecho_raw_pcb != NULL)
-  {
-    err_t err;
+  if (es != NULL) {
+    if (es->p) {
+      /* free the buffer chain if present */
+      pbuf_free(es->p);
+    }
 
-    err = tcp_bind(tcpecho_raw_pcb, IP_ANY_TYPE, 7);
-    if (err == ERR_OK)
-    {
-      tcpecho_raw_pcb = tcp_listen(tcpecho_raw_pcb);
-      tcp_accept(tcpecho_raw_pcb, tcpecho_raw_accept);
+    mem_free(es);
+  }  
+}
+
+static void
+tcpecho_raw_close(struct tcp_pcb *tpcb, struct tcpecho_raw_state *es)
+{
+  tcp_arg(tpcb, NULL);
+  tcp_sent(tpcb, NULL);
+  tcp_recv(tpcb, NULL);
+  tcp_err(tpcb, NULL);
+  tcp_poll(tpcb, NULL, 0);
+
+  tcpecho_raw_free(es);
+
+  tcp_close(tpcb);
+}
+
+static void
+tcpecho_raw_send(struct tcp_pcb *tpcb, struct tcpecho_raw_state *es)
+{
+  struct pbuf *ptr;
+  err_t wr_err = ERR_OK;
+ 
+  while ((wr_err == ERR_OK) &&
+         (es->p != NULL) && 
+         (es->p->len <= tcp_sndbuf(tpcb))) {
+    ptr = es->p;
+
+    /* enqueue data for transmission */
+    wr_err = tcp_write(tpcb, ptr->payload, ptr->len, 1);
+    if (wr_err == ERR_OK) {
+      u16_t plen;
+
+      plen = ptr->len;
+      /* continue with next pbuf in chain (if any) */
+      es->p = ptr->next;
+      if(es->p != NULL) {
+        /* new reference! */
+        pbuf_ref(es->p);
+      }
+      /* chop first pbuf from chain */
+      pbuf_free(ptr);
+      /* we can read more data now */
+      tcp_recved(tpcb, plen);
+    } else if(wr_err == ERR_MEM) {
+      /* we are low on memory, try later / harder, defer to poll */
+      es->p = ptr;
+    } else {
+      /* other problem ?? */
     }
-    else 
-    {
-      /* abort? output diagnostic? */
-    }
-  }
-  else
-  {
-    /* abort? output diagnostic? */
   }
 }
 
+static void
+tcpecho_raw_error(void *arg, err_t err)
+{
+  struct tcpecho_raw_state *es;
 
-err_t
+  LWIP_UNUSED_ARG(err);
+
+  es = (struct tcpecho_raw_state *)arg;
+
+  tcpecho_raw_free(es);
+}
+
+static err_t
+tcpecho_raw_poll(void *arg, struct tcp_pcb *tpcb)
+{
+  err_t ret_err;
+  struct tcpecho_raw_state *es;
+
+  es = (struct tcpecho_raw_state *)arg;
+  if (es != NULL) {
+    if (es->p != NULL) {
+      /* there is a remaining pbuf (chain)  */
+      tcpecho_raw_send(tpcb, es);
+    } else {
+      /* no remaining pbuf (chain)  */
+      if(es->state == ES_CLOSING) {
+        tcpecho_raw_close(tpcb, es);
+      }
+    }
+    ret_err = ERR_OK;
+  } else {
+    /* nothing to be done */
+    tcp_abort(tpcb);
+    ret_err = ERR_ABRT;
+  }
+  return ret_err;
+}
+
+static err_t
+tcpecho_raw_sent(void *arg, struct tcp_pcb *tpcb, u16_t len)
+{
+  struct tcpecho_raw_state *es;
+
+  LWIP_UNUSED_ARG(len);
+
+  es = (struct tcpecho_raw_state *)arg;
+  es->retries = 0;
+  
+  if(es->p != NULL) {
+    /* still got pbufs to send */
+    tcp_sent(tpcb, tcpecho_raw_sent);
+    tcpecho_raw_send(tpcb, es);
+  } else {
+    /* no more pbufs to send */
+    if(es->state == ES_CLOSING) {
+      tcpecho_raw_close(tpcb, es);
+    }
+  }
+  return ERR_OK;
+}
+
+static err_t
+tcpecho_raw_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err)
+{
+  struct tcpecho_raw_state *es;
+  err_t ret_err;
+
+  LWIP_ASSERT("arg != NULL",arg != NULL);
+  es = (struct tcpecho_raw_state *)arg;
+  if (p == NULL) {
+    /* remote host closed connection */
+    es->state = ES_CLOSING;
+    if(es->p == NULL) {
+      /* we're done sending, close it */
+      tcpecho_raw_close(tpcb, es);
+    } else {
+      /* we're not done yet */
+      tcpecho_raw_send(tpcb, es);
+    }
+    ret_err = ERR_OK;
+  } else if(err != ERR_OK) {
+    /* cleanup, for unknown reason */
+    if (p != NULL) {
+      pbuf_free(p);
+    }
+    ret_err = err;
+  }
+  else if(es->state == ES_ACCEPTED) {
+    /* first data chunk in p->payload */
+    es->state = ES_RECEIVED;
+    /* store reference to incoming pbuf (chain) */
+    es->p = p;
+    tcpecho_raw_send(tpcb, es);
+    ret_err = ERR_OK;
+  } else if (es->state == ES_RECEIVED) {
+    /* read some more data */
+    if(es->p == NULL) {
+      es->p = p;
+      tcpecho_raw_send(tpcb, es);
+    } else {
+      struct pbuf *ptr;
+
+      /* chain pbufs to the end of what we recv'ed previously  */
+      ptr = es->p;
+      pbuf_cat(ptr,p);
+    }
+    ret_err = ERR_OK;
+  } else {
+    /* unkown es->state, trash data  */
+    tcp_recved(tpcb, p->tot_len);
+    pbuf_free(p);
+    ret_err = ERR_OK;
+  }
+  return ret_err;
+}
+
+static err_t
 tcpecho_raw_accept(void *arg, struct tcp_pcb *newpcb, err_t err)
 {
   err_t ret_err;
@@ -120,8 +263,7 @@ tcpecho_raw_accept(void *arg, struct tcp_pcb *newpcb, err_t err)
   tcp_setprio(newpcb, TCP_PRIO_MIN);
 
   es = (struct tcpecho_raw_state *)mem_malloc(sizeof(struct tcpecho_raw_state));
-  if (es != NULL)
-  {
+  if (es != NULL) {
     es->state = ES_ACCEPTED;
     es->pcb = newpcb;
     es->retries = 0;
@@ -133,225 +275,29 @@ tcpecho_raw_accept(void *arg, struct tcp_pcb *newpcb, err_t err)
     tcp_poll(newpcb, tcpecho_raw_poll, 0);
     tcp_sent(newpcb, tcpecho_raw_sent);
     ret_err = ERR_OK;
-  }
-  else
-  {
+  } else {
     ret_err = ERR_MEM;
   }
   return ret_err;
 }
 
-err_t
-tcpecho_raw_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err)
-{
-  struct tcpecho_raw_state *es;
-  err_t ret_err;
-
-  LWIP_ASSERT("arg != NULL",arg != NULL);
-  es = (struct tcpecho_raw_state *)arg;
-  if (p == NULL)
-  {
-    /* remote host closed connection */
-    es->state = ES_CLOSING;
-    if(es->p == NULL)
-    {
-       /* we're done sending, close it */
-       tcpecho_raw_close(tpcb, es);
-    }
-    else
-    {
-      /* we're not done yet */
-      tcpecho_raw_send(tpcb, es);
-    }
-    ret_err = ERR_OK;
-  }
-  else if(err != ERR_OK)
-  {
-    /* cleanup, for unkown reason */
-    if (p != NULL)
-    {
-      pbuf_free(p);
-    }
-    ret_err = err;
-  }
-  else if(es->state == ES_ACCEPTED)
-  {
-    /* first data chunk in p->payload */
-    es->state = ES_RECEIVED;
-    /* store reference to incoming pbuf (chain) */
-    es->p = p;
-    tcpecho_raw_send(tpcb, es);
-    ret_err = ERR_OK;
-  }
-  else if (es->state == ES_RECEIVED)
-  {
-    /* read some more data */
-    if(es->p == NULL)
-    {
-      es->p = p;
-      tcpecho_raw_send(tpcb, es);
-    }
-    else
-    {
-      struct pbuf *ptr;
-
-      /* chain pbufs to the end of what we recv'ed previously  */
-      ptr = es->p;
-      pbuf_cat(ptr,p);
-    }
-    ret_err = ERR_OK;
-  }
-  else
-  {
-    /* unkown es->state, trash data  */
-    tcp_recved(tpcb, p->tot_len);
-    pbuf_free(p);
-    ret_err = ERR_OK;
-  }
-  return ret_err;
-}
-
 void
-tcpecho_raw_error(void *arg, err_t err)
+tcpecho_raw_init(void)
 {
-  struct tcpecho_raw_state *es;
+  tcpecho_raw_pcb = tcp_new_ip_type(IPADDR_TYPE_ANY);
+  if (tcpecho_raw_pcb != NULL) {
+    err_t err;
 
-  LWIP_UNUSED_ARG(err);
-
-  es = (struct tcpecho_raw_state *)arg;
-
-  tcpecho_raw_free(es);
-}
-
-err_t
-tcpecho_raw_poll(void *arg, struct tcp_pcb *tpcb)
-{
-  err_t ret_err;
-  struct tcpecho_raw_state *es;
-
-  es = (struct tcpecho_raw_state *)arg;
-  if (es != NULL)
-  {
-    if (es->p != NULL)
-    {
-      /* there is a remaining pbuf (chain)  */
-      tcpecho_raw_send(tpcb, es);
+    err = tcp_bind(tcpecho_raw_pcb, IP_ANY_TYPE, 7);
+    if (err == ERR_OK) {
+      tcpecho_raw_pcb = tcp_listen(tcpecho_raw_pcb);
+      tcp_accept(tcpecho_raw_pcb, tcpecho_raw_accept);
+    } else {
+      /* abort? output diagnostic? */
     }
-    else
-    {
-      /* no remaining pbuf (chain)  */
-      if(es->state == ES_CLOSING)
-      {
-        tcpecho_raw_close(tpcb, es);
-      }
-    }
-    ret_err = ERR_OK;
+  } else {
+    /* abort? output diagnostic? */
   }
-  else
-  {
-    /* nothing to be done */
-    tcp_abort(tpcb);
-    ret_err = ERR_ABRT;
-  }
-  return ret_err;
-}
-
-err_t
-tcpecho_raw_sent(void *arg, struct tcp_pcb *tpcb, u16_t len)
-{
-  struct tcpecho_raw_state *es;
-
-  LWIP_UNUSED_ARG(len);
-
-  es = (struct tcpecho_raw_state *)arg;
-  es->retries = 0;
-  
-  if(es->p != NULL)
-  {
-    /* still got pbufs to send */
-    tcp_sent(tpcb, tcpecho_raw_sent);
-    tcpecho_raw_send(tpcb, es);
-  }
-  else
-  {
-    /* no more pbufs to send */
-    if(es->state == ES_CLOSING)
-    {
-      tcpecho_raw_close(tpcb, es);
-    }
-  }
-  return ERR_OK;
-}
-
-void
-tcpecho_raw_send(struct tcp_pcb *tpcb, struct tcpecho_raw_state *es)
-{
-  struct pbuf *ptr;
-  err_t wr_err = ERR_OK;
- 
-  while ((wr_err == ERR_OK) &&
-         (es->p != NULL) && 
-         (es->p->len <= tcp_sndbuf(tpcb)))
-  {
-  ptr = es->p;
-
-  /* enqueue data for transmission */
-  wr_err = tcp_write(tpcb, ptr->payload, ptr->len, 1);
-  if (wr_err == ERR_OK)
-  {
-     u16_t plen;
-
-     plen = ptr->len;
-     /* continue with next pbuf in chain (if any) */
-     es->p = ptr->next;
-     if(es->p != NULL)
-     {
-       /* new reference! */
-       pbuf_ref(es->p);
-     }
-     /* chop first pbuf from chain */
-     pbuf_free(ptr);
-     /* we can read more data now */
-     tcp_recved(tpcb, plen);
-   }
-   else if(wr_err == ERR_MEM)
-   {
-      /* we are low on memory, try later / harder, defer to poll */
-     es->p = ptr;
-   }
-   else
-   {
-     /* other problem ?? */
-   }
-  }
-}
-
-void
-tcpecho_raw_close(struct tcp_pcb *tpcb, struct tcpecho_raw_state *es)
-{
-  tcp_arg(tpcb, NULL);
-  tcp_sent(tpcb, NULL);
-  tcp_recv(tpcb, NULL);
-  tcp_err(tpcb, NULL);
-  tcp_poll(tpcb, NULL, 0);
-
-  tcpecho_raw_free(es);
-
-  tcp_close(tpcb);
-}
-
-void tcpecho_raw_free(struct tcpecho_raw_state *es)
-{
-  if (es != NULL)
-  {
-    if (es->p)
-    {
-      /* free the buffer chain if present */
-      pbuf_free(es->p);
-    }
-
-    mem_free(es);
-  }  
 }
 
 #endif /* LWIP_TCP */
--- apps/udpecho_raw/udpecho_raw.c
+++ apps/udpecho_raw/udpecho_raw.c
@@ -44,7 +44,6 @@
  *
  */
 
-
 #include "lwip/opt.h"
 #include "lwip/debug.h"
 #include "lwip/stats.h"
@@ -55,35 +54,7 @@
 
 static struct udp_pcb *udpecho_raw_pcb;
 
-void udpecho_raw_recv(void *arg, struct udp_pcb *upcb, struct pbuf *p,
-                      const ip_addr_t *addr, u16_t port);
-
-void
-udpecho_raw_init(void)
-{
-  udpecho_raw_pcb = udp_new_ip_type(IPADDR_TYPE_ANY);
-  if (udpecho_raw_pcb != NULL)
-  {
-    err_t err;
-
-    err = udp_bind(udpecho_raw_pcb, IP_ANY_TYPE, 7);
-    if (err == ERR_OK)
-    {
-      udp_recv(udpecho_raw_pcb, udpecho_raw_recv, NULL);
-    }
-    else
-    {
-      /* abort? output diagnostic? */
-    }
-  }
-  else
-  {
-    /* abort? output diagnostic? */
-  }
-}
-
-
-void
+static void
 udpecho_raw_recv(void *arg, struct udp_pcb *upcb, struct pbuf *p,
                  const ip_addr_t *addr, u16_t port)
 {
@@ -93,6 +64,24 @@ udpecho_raw_recv(void *arg, struct udp_pcb *upcb, struct pbuf *p,
     udp_sendto(upcb, p, addr, port);
     /* free the pbuf */
     pbuf_free(p);
+  }
+}
+
+void
+udpecho_raw_init(void)
+{
+  udpecho_raw_pcb = udp_new_ip_type(IPADDR_TYPE_ANY);
+  if (udpecho_raw_pcb != NULL) {
+    err_t err;
+
+    err = udp_bind(udpecho_raw_pcb, IP_ANY_TYPE, 7);
+    if (err == ERR_OK) {
+      udp_recv(udpecho_raw_pcb, udpecho_raw_recv, NULL);
+    } else {
+      /* abort? output diagnostic? */
+    }
+  } else {
+    /* abort? output diagnostic? */
   }
 }
 
